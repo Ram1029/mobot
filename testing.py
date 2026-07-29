f857853142ffedeff17ad1f302a7a8d3ddf20e82
Pram1029 <140921844+Ram1029@users.noreply.github.com>
2026-07-29 15:06:34 +0300

    testing rework

--- testing.py
+++ testing.py
@@ -3,25 +3,18 @@ dotenv.load_dotenv('.env')
 
 import os
 import asyncio
-from cloud.fsm import FiniteStatesStorage, MessageStorage, create_message_storage, get_message_storage
-
-from db.connection import indev_connection
+from mobot.create_bot import create_bot
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def main():
-    driver, pool = await indev_connection()
-    create_message_storage(MessageStorage(pool=pool))
-
-    from mobot.create_bot import create_bot
-    bot, dp = create_bot(bot_token=BOT_TOKEN, bot_storage=FiniteStatesStorage(pool=pool))
+    bot, dp = create_bot(bot_token=BOT_TOKEN)
 
     print('Запуск longpolling...')
     try:
         await dp.start_polling(bot)
     except:
         await dp.storage.close()
-        await get_message_storage().close()
 
 if __name__ == "__main__":
     asyncio.run(main())
